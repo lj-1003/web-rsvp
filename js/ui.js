a62d636d41c3684e4017f5340a77aa68451881e6
lj-1003 <levijustin24@gmail.com>
2026-01-11 20:54:52 -0600

    Response buttons return values.

--- js/ui.js
+++ js/ui.js
@@ -8,7 +8,7 @@ import {
   getGuest,
 } from "../js/firebaseDB.js"
 
-
+//------------------------------------------------
 //Create Guests
 async function createGuest() {
   const inputs = [
@@ -24,7 +24,7 @@ async function createGuest() {
 
   inputs.forEach((name, index) => {
     if (name && name.trim() !== "") {
-      guestData[`name${index + 1}Input`] = name;
+      guestData[`name${index + 1}`] = name;
       guestData[`name${index + 1}Response`] = "Not responded";
     }
   });
@@ -39,9 +39,8 @@ async function createGuest() {
 
   window.location.replace("../pages/admin.html");
 }
-
-
-
+//------------------------------------------------
+//------------------------------------------------
 // Load full guest list for admin page
 async function getGuestData() {
   const guests = await getGuest();
@@ -58,9 +57,29 @@ async function getGuestData() {
     
   });
 }
-
 getGuestData();
+//------------------------------------------------
+//------------------------------------------------
+//Update RSVP
+async function updateRSVP(id, response) {
+  console.log(response);
+  console.log(id);
+}
 
+async function checkResponseYes(id) {
+  console.log("User selected yes")
+  const response = true;
+  updateRSVP(id, response);
+}
+
+// Update RSVP response
+async function checkResponseNo(id) {
+  console.log("User selected no")
+  const response = false;
+  updateRSVP(id, response);
+}
+//------------------------------------------------
+//------------------------------------------------
 // Loads RSVP for public page
 let found
 async function getPersonalData() {
@@ -70,24 +89,77 @@ async function getPersonalData() {
 
   const guests = await getGuest();
 
-  const name = document.getElementById("rsvpName");
-  const plus = document.getElementById("rsvpPlus");
-
-  name.innerHTML = ""; // Clears previous items
-  plus.innerHTML = ""; // Clears previous items
+  const rsvpName = document.getElementById("rsvpName");
+  rsvpName.innerHTML = ""; // Clears previous items
 
   guests.forEach(guest => {
     if (guest.id == idInput) {
       found = true;
-      name.textContent = `Guest 1: ${guest.name1Input}`
+
+      const names = [
+        guest.name1,
+        guest.name1Response,
+
+        guest.name2,
+        guest.name2Response,
+
+        guest.name3,
+        guest.name3Response,
+
+        guest.name4,
+        guest.name4Response,
+
+        guest.name5,
+        guest.name5Response,
+
+        guest.name6,
+        guest.name6Response,
+      ];
+      
+      names.forEach((name, index) => {
+        //lets these varibales work in bigger scope
+        let li;
+        let yesButton;
+        let noButton;
+
+        if(name) { //Filters out blank name inputs
+          //Creates a list item
+          li = document.createElement("li");
+          li.textContent = `${name}`;
+
+        if (index % 2 === 1) { //checks if index is odd so buttons are only applied to responses
+          //yes button
+          yesButton = document.createElement("button");
+          yesButton.innerHTML = '<i>RSVP Yes</i>';
+          yesButton.addEventListener("click", async () => {
+            await checkResponseYes(guest.id); //triggers update functionality 
+          })
+          li.appendChild(yesButton); //puts button inside li
+          
+          //no button
+          noButton = document.createElement("button");
+          noButton.innerHTML = '<i>RSVP No</i>';
+          noButton.addEventListener("click", async () => {
+            await checkResponseNo(guest.id); //triggers update functionality 
+          })
+          li.appendChild(noButton); //puts buttons inside li
+
+        } else {
+          console.log("Are you reading this?");
+        }
+          rsvpName.appendChild(li); //buttons now inside li -> goes into ul
+        };
+      });
+      console.log(names);
     }
   });
 
   //ID found flag 
   if (!found) {
-      name.textContent = "RSVP not found. Retry code, or contact Levi for support."
+      rsvpName.textContent = "RSVP not found. Retry code, or contact Levi for support."
     }
 }
+//------------------------------------------------
 
 window.resetForm = resetForm;
 window.createGuest = createGuest;
